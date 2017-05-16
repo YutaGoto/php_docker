af1fa79dfd7570ecf29e065807260b2a1fd92e3b
YutaGoto <you.goto.510@gmail.com>
2017-05-16 16:53:11 +0900

    init commit

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>index.php</title>
+  </head>
+
+  <body>
+    <h1>Hello World!</h1>
+    <p>
+      <?php
+        echo "I Am Running PHP " . phpversion() . " With Apache And Docker";
+      ?>
+    </p>
+  </body>
+</html>
